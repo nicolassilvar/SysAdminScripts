009f96cb8be370c2cf941169120e2b97517cda9f
nicolassilvar <nicolas.silva.r@gmail.com>
2020-05-20 17:19:05 -0500

    Script add

---
+++ dotfiles.py
@@ -0,0 +1,29 @@
+import subprocess
+
+def removeDot(name):
+    if name[0] == '.':
+        return name[1:]
+    else:
+        return name
+
+dotfiles = { \
+        '.alias':'/home/nico/',\
+        '.bash_profile':'/home/nico/',\
+        '.bashrc':'/home/nico/',\
+        '.env':'/home/nico/',\
+        '.functions':'/home/nico/',\
+        '.gitconfig':'/home/nico/',\
+        'config':'/home/nico/.config/i3/',\
+        '.inputrc':'/home/nico/',\
+        '.prompt':'/home/nico/',\
+        'vimrc':'/home/nico/.vim/'}
+
+dotDir = '/home/nico/.dotfiles/'
+
+for name,path in dotfiles.items():
+    subprocess.run(f"cp {path+name} {removeDot(name)}", shell=True, cwd=dotDir)
+    subprocess.run(f"git add {removeDot(name)}", shell=True, cwd=dotDir)
+    subprocess.run(f"git commit -m 'Update'", shell=True, cwd=dotDir)
+    subprocess.run(f"git push -u origin master", shell=True, cwd=dotDir)
+
+
