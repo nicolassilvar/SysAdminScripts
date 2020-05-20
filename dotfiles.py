65d9f215543250630dfc3512af4cb2f0983ec99a
nicolassilvar <nicolas.silva.r@gmail.com>
2020-05-20 18:07:16 -0500

    No need to update if files no change

--- dotfiles.py
+++ dotfiles.py
@@ -1,4 +1,5 @@
 import subprocess
+import filecmp
 
 def removeDot(name):
     if name[0] == '.':
@@ -21,9 +22,14 @@ dotfiles = { \
 dotDir = '/home/nico/.dotfiles/'
 
 for name,path in dotfiles.items():
-    subprocess.run(f"cp {path+name} {removeDot(name)}", shell=True, cwd=dotDir)
-    subprocess.run(f"git add {removeDot(name)}", shell=True, cwd=dotDir)
-    subprocess.run(f"git commit -m 'Update'", shell=True, cwd=dotDir)
-    subprocess.run(f"git push -u origin master", shell=True, cwd=dotDir)
+    print(f'Working on file {path+name}')
+
+    if not filecmp.cmp(f'{dotDir + removeDot(name)}',f'{path + name}'):
+        subprocess.run(f"cp {path+name} {removeDot(name)}", shell=True, cwd=dotDir)
+        subprocess.run(f"git add {removeDot(name)}", shell=True, cwd=dotDir)
+        subprocess.run(f"git commit -m 'Update'", shell=True, cwd=dotDir)
+        subprocess.run(f"git push -u origin master", shell=True, cwd=dotDir)
+    else:
+        print("All files are equal to source")
 
 
